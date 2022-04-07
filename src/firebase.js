190561d5a800af264cf3ff664e9ecb2a65972251
Dmitry Bazhanov <dmitry_bazhanov@Dmitrys-MacBook-Pro.local>
2022-04-07 21:49:11 +0300

    Type fixes

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AITH_DOMAIN,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGENIG_SENDER_ID,
