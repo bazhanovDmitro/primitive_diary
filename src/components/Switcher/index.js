907797b2d98897ddd1e33af1db458ca7f3530827
Dmitry Bazhanov <dmitry_bazhanov@Dmitrys-MacBook-Pro.local>
2022-04-07 18:05:23 +0300

    Mode buttons added

---
+++ src/components/Switcher/index.js
@@ -0,0 +1,19 @@
+import s from "./switcher.module.scss";
+
+export default function Switcher({ style, buttons, visible, activeButton }) {
+  if (visible) {
+    return (
+      <div className={style ? style.switcher : s.switcher}>
+        {buttons.map((button) => (
+          <button
+            className={activeButton === button.text ? s.active : s.button}
+            onClick={button.onClick}
+          >
+            {button.text}
+          </button>
+        ))}
+      </div>
+    );
+  }
+  return null;
+}
